3f9e4108bc9f438e7b07bc547b439d7de59c287b
tabbykatz <tomelay@gmail.com>
2021-08-26 13:48:19 -0700

    add data refinery

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import * as React from 'react';
-import { backgroundSwitcher, forecastParser, dateBuilder } from './helpers';
-
+import * as React from "react";
+import { responseParser } from "./helpers";
 
 const api = {
   base: "https://api.openweathermap.org/data/2.5/forecast?q=",
-  iconBase: " http://openweathermap.org/img/wn/"
-}
-
+  iconBase: " http://openweathermap.org/img/wn/",
+};
 
 function App() {
-  let [query, setQuery] = React.useState('');
+  let [query, setQuery] = React.useState("");
   let [weather, setWeather] = React.useState({});
-  
-  const shouldDisplayForecast = Object.keys(weather).length > 0;
-  console.log({weather, shouldDisplayForecast})
 
-  
+  let shouldDisplayForecast = Object.keys(weather).length > 0;
+  console.log({ weather, shouldDisplayForecast });
+
   const search = () => {
-      fetch(`${api.base}${query}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
-          setWeather(data)
-    })
-}
-const onSubmit = (e) => {
-  e.preventDefault();
-  search()
-}
+    fetch(
+      `${api.base}${query}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+        setWeather(responseParser(data));
+      });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    search();
+  };
 
-    return (
-    <div className='app'>
+  return (
+    <div className="app">
       <main>
         <form onSubmit={onSubmit} className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Enter a city name..."
-            onChange={e => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}
             value={query}
           />
         </form>
-        { shouldDisplayForecast ? 
-        <pre>{JSON.stringify(weather, null, 2)}</pre> : null }
+        {shouldDisplayForecast ? (
+          <pre>{JSON.stringify(weather, null, 2)}</pre>
+        ) : null}
       </main>
-    </div>)
-  
+    </div>
+  );
 }
-
 
 export default App;
--- src/helpers.js
+++ src/helpers.js
@@ -1,32 +1,34 @@
+export const responseParser = (data) => {
+  let newState = {};
+  newState.location = `${data.city.name}, ${data.city.country}`;
 
-export const dateBuilder = () => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let d = new Date();
+  let tempsByDate = {};
+  let dataByDate = {};
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${month} ${date}, ${year}`
-  }
-
-export const forecastParser = (data) => {
-    let fiveDays = {}
-    for (let i = 0; i < data.list.length; i++) {
-        let day = data.list[i].dt_txt.substring(5, 10);
-        if (!fiveDays[day] && data.list[i].dt_txt.includes("18:00:00")) {
-            fiveDays[day] = data.list[i];
-        }
+  for (let i = 0; i < data.list.length; i++) {
+    let day = data.list[i].dt_txt.substring(0, 10);
+    let icon, main;
+    if (!tempsByDate[day]) {
+      tempsByDate[day] = [data.list[i].main.temp];
+    } else {
+      tempsByDate[day].push(data.list[i].main.temp);
     }
-    return fiveDays
-}
-
-
-  const backgroundSwitcher = (weather) => {
-    const types = {"Thunderstorm": "thunder.jpeg", "Drizzle": "drizzle.jpeg" , "Rain": "rain.jpeg", "Snow": "snow.jpeg", "Clear": "clear.jpeg", "Clouds": "clouds.jpeg", "Mist": "mist.jpeg", "Smoke": "smoke.jpeg", "Haze": "haze.jpeg", "Dust": "dust.jpeg", "Fog": "fog.jpeg", "Sand": "sand.jpeg", "Ash": "ash.jpeg", "Squall": "squall.jpeg", "Tornado": "tornado.jpeg"};
-    let weatherType = weather.weather[0].main;
-    console.log(weatherType)
-    document.querySelector('.app').style.backgroundImage = `url(./assets/${types[weatherType]}`;
+    if (!dataByDate[day]) {
+      dataByDate[day] = [
+        data.list[i].weather[0].main,
+        data.list[i].weather[0].icon,
+      ];
+    }
   }
+
+  const keys = Object.keys(tempsByDate);
+
+  keys.forEach((key, index) => {
+    newState[key] = [
+      tempsByDate[key].reduce((temp, sum) => temp + sum, 0) /
+        tempsByDate[key].length,
+      ...dataByDate[key],
+    ];
+  });
+  return newState;
+};
